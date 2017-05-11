d9767643f51afc92165b1b19c8657dcbab1163f5
mpiquero <mpiquero7164@gmail.com>
2017-05-11 16:33:42 -0400

    aded get

--- server/server.js
+++ server/server.js
@@ -1,32 +1,55 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/user.js');
+var {User} = require('./models/user');
 
-var app =express();
+var app = express();
 
 app.use(bodyParser.json());
 
-//going to use post to setup a route using the / for posting
-app.post('/todos', (request, response) => {
-  console.log(request.body);
+app.post('/todos', (req, res) => {
   var todo = new Todo({
-    text: request.body.text
+    text: req.body.text
   });
 
   todo.save().then((doc) => {
-    //success
-    response.send(doc);
+    res.send(doc);
   }, (e) => {
-    //error
-    response.status(400).send(e);
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
 app.listen(3000, () => {
-  console.log('Server started at 3000');
-})
+  console.log('Started on port 3000');
+});
 
-module.exports = { app };
+module.exports = {app};
