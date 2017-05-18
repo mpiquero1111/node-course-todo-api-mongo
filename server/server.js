13d68a4c4cc46de8d067ed2a25ceaffdf397df42
mpiquero <mpiquero7164@gmail.com>
2017-05-18 15:55:33 -0400

    added login

--- server/server.js
+++ server/server.js
@@ -119,6 +119,20 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+//login
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
